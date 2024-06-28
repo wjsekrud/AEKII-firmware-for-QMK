db1d074f268ce08256df7752280347f9f639bc9f
wjsekrud <qquark4187@gmail.com>
2024-06-28 17:53:59 +0900

    hex
    compile fix / hex file

--- aekii/config.h
+++ aekii/config.h
@@ -21,8 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define VENDOR_ID 0x4949
 #define PRODUCT_ID 0x0021
 #define DEVICE_VER 0x0001
-#define MANUFACTURER Apple
-#define PRODUCT AEKII
+#define MANUFACTURER "Apple"
+#define PRODUCT "AEKII"
 
 /* key matrix size */
 #define MATRIX_ROWS 8
--- aekii/rules.mk
+++ aekii/rules.mk
@@ -1,20 +1,22 @@
 MCU = atmega32a
 F_CPU = 16000000
 
-BOOTLOADER = bootloadHID
+BOOTLOADER = bootloadhid
 BOOTLOADER_SIZE = 4096
 
 OPT_DEFS += -DBOOTLOADER_SIZE=$(BOOTLOADER_SIZE)
+FORCE_LAYOUT=yes
 
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
-MOUSEKEY_ENABLE = yes	# Mouse keys
+#BOOTMAGIC_ENABLE = lite	# Virtual DIP switch configuration
+MOUSEKEY_ENABLE = no	# Mouse keys
 EXTRAKEY_ENABLE = yes	# Audio control and System control
-CONSOLE_ENABLE = yes	# Console for debug
+CONSOLE_ENABLE = no	# Console for debug
 COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = no   # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no	    # USB Nkey Rollover
 BACKLIGHT_ENABLE = no
 RGBLIGHT_ENABLE = no
+CUSTOM_MATRIX = lite
